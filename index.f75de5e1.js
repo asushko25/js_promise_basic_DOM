f1107d4af8b69a1889eaafca6d641e0c6a0613c7
Oleksandr Sushko <alexandrsushko25@gmail.com>
2024-07-18 22:10:08 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,32 @@
+"use strict";
+const promise1 = new Promise((resolve, reject)=>{
+    const logo = document.querySelector(".logo");
+    logo.addEventListener("click", ()=>{
+        resolve("Success");
+    });
+});
+const promise2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        reject(new Error("Error"));
+    }, 3000);
+});
+function addMessage(message, isError = false) {
+    const body = document.body;
+    const div = document.createElement("div");
+    div.classList.add("message");
+    if (isError) div.classList.add("error-message");
+    div.textContent = message;
+    body.appendChild(div);
+}
+promise1.then(()=>{
+    addMessage("Promise was resolved!");
+}).catch(()=>{
+    addMessage("Promise was rejected!", true);
+});
+promise2.then(()=>{
+    addMessage("Promise was resolved!");
+}).catch(()=>{
+    addMessage("Promise was rejected!", true);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
